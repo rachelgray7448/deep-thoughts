b470f8759e95cd47d3390bbc697978d88b9959a3
Rachel <rachel.gray7448@gmail.com>
2022-07-03 14:22:07 -0400

    added mutations

--- server/server.js
+++ server/server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
+const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: authMiddleware
 });
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
